2eac8f1da9398c68a5a4eeb3610a56730059349d
sashkow <lykhenko.olexandr@gmail.com>
2018-07-20 18:02:04 +0300

    social auth

--- citizen_engagement_portal/settings.py
+++ citizen_engagement_portal/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+from password import getpass
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -37,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'social_django'
 ]
 
 MIDDLEWARE = [
@@ -73,13 +76,17 @@ WSGI_APPLICATION = 'citizen_engagement_portal.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'voluteer_db',
+        'USER': 'volunteer_user',
+        'PASSWORD': getpass(),
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
