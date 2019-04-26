4a8270221d3b7c6d9a86deb46f645e67677b4537
sashkow <lykhenko.olexandr@gmail.com>
2019-04-26 17:33:01 +0000

    custom notification, media files

--- volunteer/management/commands/custom_notification.py
+++ volunteer/management/commands/custom_notification.py
@@ -1,5 +1,5 @@
 from django.core.management.base import BaseCommand, CommandError
-from volunteer.models import Event, User
+from volunteer.models import Event, User, EventsParticipant
 from djgeojson.fields import  PointField
 
 from notifications.signals import notify
@@ -18,22 +18,21 @@ class Command(BaseCommand):
     #     parser.add_argument('poll_id', nargs='+', type=int)
 
     def handle(self, *args, **options):
+        users = EventsParticipant.objects.filter(event__name = "Різдвяний ярмарок").values_list('user', flat=True)
+
+        for usr in users:
+            django_user = User.objects.get(id=usr).django_user_id
+            event_instance = Event.objects.get(name = "Різдвяний ярмарок")
+            message = "Доброго дня! Мене звати Ксенія. Я організатор Різдвяного ярмарку. Дякуємо, що відгукнулися стати волонтером. Напишіть, будь ласка, на ksenia.kosyuk@gmail.com або зателефонуйте 0978933516."
 
 
-        django_user = User.objects.get(first_name = "Сашко", last_name="Виконавець").django_user_id
-
-        event_instance = Event.objects.get(name = "Різдвяний ярмарок")
-
-        message = "Доброго дня! Мене звати Ксенія. Я організатор Різдвяного ярмарку. Дякуємо, що відгукнулися стати волонтером. Напишіть, будь ласка, на ksenia.kosyuk@gmail.com або зателефонуйте 0978933516."
-
-
-        notify.send(
-            django_user,
-            recipient=django_user,
-            verb="custom",
-            target=event_instance,
-            # timestamp = datetime.datetime.now().strftime("$d %B %Y %h:%m"),
-            data={'type': '6', 'message': message},  # Власне сповіщення
-        )
+            notify.send(
+                django_user,
+                recipient=django_user,
+                verb="custom",
+                target=event_instance,
+                # timestamp = datetime.datetime.now().strftime("$d %B %Y %h:%m"),
+                data={'type': '6', 'message': message},  # Власне сповіщення
+            )
 
 
