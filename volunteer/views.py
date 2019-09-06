21e884788d8f681014cc87d696eaffa8ea65df7f
darya <daryaliha@gmail.com>
2019-09-07 02:10:32 +0300

    search styles

--- volunteer/views.py
+++ volunteer/views.py
@@ -208,7 +208,7 @@ def profile(request):
     max_user_point = user_points.aggregate(Max('quantity'))
 
     types_events = EventsType.objects.all()
-    status_events = Status.objects.all()
+    status_events = Status.objects.all()[:2]
     cities = City.objects.all()
 
 
